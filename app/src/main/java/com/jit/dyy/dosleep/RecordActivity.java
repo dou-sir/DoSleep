852eb97a089590c149aecb190665e3145bd349f1
dou-sir <1403268725@qq.com>
2021-03-30 22:24:33 +0800

    test

--- app/src/main/java/com/jit/dyy/dosleep/RecordActivity.java
+++ app/src/main/java/com/jit/dyy/dosleep/RecordActivity.java
@@ -1,15 +1,22 @@
 package com.jit.dyy.dosleep;
 
 import android.Manifest;
+import android.animation.ObjectAnimator;
+import android.annotation.SuppressLint;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.app.TimePickerDialog;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.BitmapFactory;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -17,19 +24,35 @@ import android.hardware.SensorManager;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.IBinder;
 import android.os.Message;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 import java.util.TimeZone;
 
 import butterknife.BindView;
@@ -38,7 +61,7 @@ import butterknife.OnClick;
 
 import static java.lang.Math.pow;
 
-public class RecordActivity extends AppCompatActivity {
+public class RecordActivity extends AppCompatActivity implements View.OnClickListener {
 
 
     @BindView(R.id.word1)
@@ -65,12 +88,48 @@ public class RecordActivity extends AppCompatActivity {
     Button start;
     @BindView(R.id.reset)
     Button reset;
-    @BindView(R.id.imageView)
-    ImageView imageView;
     @BindView(R.id.ibClose)
     ImageButton ibClose;
     @BindView(R.id.linearLayout)
     LinearLayout linearLayout;
+    @BindView(R.id.rlMain)
+    RelativeLayout rlMain;
+    @BindView(R.id.image1)
+    ImageView imageView;
+    @BindView(R.id.play_button)
+    ImageButton imageButton;
+    @BindView(R.id.last_music)
+    ImageButton last_music;
+    @BindView(R.id.next_music)
+    ImageButton next_music;
+    @BindView(R.id.rl1)
+    RelativeLayout rl1;
+    @BindView(R.id.loaded)
+    TextView loaded;
+    @BindView(R.id.source)
+    TextView source;
+    @BindView(R.id.playing)
+    TextView playing;
+    @BindView(R.id.playTime)
+    TextView playTime;
+    @BindView(R.id.seekbar)
+    SeekBar seekBar;
+    @BindView(R.id.resTime)
+    TextView resTime;
+    @BindView(R.id.rl2)
+    RelativeLayout rl2;
+    @BindView(R.id.musicList)
+    ListView musicList;
+
+    private List<Music> music = new LinkedList<>();
+    private MusicService musicService;
+    private boolean isPlaying;
+    private SimpleDateFormat time = new SimpleDateFormat("mm:ss", Locale.getDefault());
+    private ObjectAnimator animator;
+    private int musicPosition = 0;
+
+    public static String musicWidget = "com.jit.dyy.dosleep.musicOption";
+    public static String changeWidget = "com.jit.dyy.dosleep.changeWidget";
 
     private long timeusedinsec;
     private boolean isstop = false;
@@ -111,12 +170,46 @@ public class RecordActivity extends AppCompatActivity {
 
     };
 
+    Handler handler = new Handler();
+    Runnable update = new Runnable() {
+        @Override
+        public void run() {
+            int current = musicService.getMediaPlayer().getCurrentPosition();
+            int res = musicService.getMediaPlayer().getDuration() - current;
+            seekBar.setProgress(current);
+            playTime.setText(time.format(current));
+            resTime.setText(time.format(res));
+            if (res <= 0) {
+                /*musicService.stop();
+                playing.setText(R.string.stop);
+//                play.setText(R.string.play);
+                imageButton.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.bofangzhuangtaitingzhi));
+                isPlaying = false;
+                animator.end();
+                seekBar.setProgress(0);
+                handler.removeCallbacks(update);*/
+                setNext_music();
+            }
+            handler.postDelayed(update, 100);
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
         ButterKnife.bind(this);
+        //connection();
+        prepareMusic();
+        setListView();
+        setAnimator();
+        connection();
+        setSeekBarOption();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(musicWidget);
+        registerReceiver(receiver, intentFilter);
         init();
+
     }
 
     private void init() {
@@ -133,9 +226,12 @@ public class RecordActivity extends AppCompatActivity {
                 ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
         }//todo
 
+
+
+
     }
 
-    @OnClick({R.id.reset, R.id.start, R.id.timeBtn, R.id.ibClose})
+    @OnClick({R.id.reset, R.id.start, R.id.timeBtn, R.id.ibClose,R.id.play_button, R.id.next_music, R.id.last_music})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.reset:
@@ -216,8 +312,19 @@ public class RecordActivity extends AppCompatActivity {
                 alarmManager.cancel(pendingIntent);
 //                cancelAlarmBtn.setVisibility(View.INVISIBLE);
                 break;
+            case R.id.play_button:
+                playAndPause();
+                break;
+            case R.id.next_music:
+                setNext_music();
+                break;
+            case R.id.last_music:
+                setLast_music();
+                break;
             case R.id.ibClose:
-                this.finish();
+                isPlaying = false;
+                musicService.playAndPause();
+                finish();
                 break;
         }
     }
@@ -399,6 +506,245 @@ public class RecordActivity extends AppCompatActivity {
         cv.put("sumOfSleep", sleepHour);
         cv.put("timeOfSleep", timeOfSleep);
         sqLiteDatabase.insert("grade_table", null, cv);
+    }
+
+
+    //todo music
+    private void prepareMusic() {
+        music.add(new Music("music1", R.raw.music1));
+        music.add(new Music("music2", R.raw.music2));
+        music.add(new Music("music3", R.raw.music3));
+        music.add(new Music("music4", R.raw.music4));
+        music.add(new Music("music5", R.raw.music5));
+        music.add(new Music("music6", R.raw.music6));
+        music.add(new Music("music7", R.raw.music7));
+        music.add(new Music("music8", R.raw.music8));
+        music.add(new Music("music9", R.raw.music9));
+        music.add(new Music("music10", R.raw.music10));
+        music.add(new Music("music11", R.raw.music11));
+        music.add(new Music("music12", R.raw.music12));
+        music.add(new Music("music13", R.raw.music13));
+        music.add(new Music("music14", R.raw.music14));
+        music.add(new Music("music15", R.raw.music15));
+        music.add(new Music("music16", R.raw.music16));
+        music.add(new Music("music17", R.raw.music17));
+    }
+
+    private void setListView() {
+        List<Map<String, String>> ml = new ArrayList<>();
+        for (Music i : music) {
+            Map<String, String> temp = new HashMap<>();
+            temp.put("musicName", i.getMusicName());
+            ml.add(temp);
+        }
+        SimpleAdapter sa = new SimpleAdapter(this, ml, R.layout.music_list,
+                new String[]{"musicName"}, new int[]{R.id.text});
+        musicList.setAdapter(sa);
+
+        musicList.setOnTouchListener(new View.OnTouchListener() {
+
+            public boolean onTouch(View v, MotionEvent event) {
+                // TODO Auto-generated method stub
+                musicList.getParent().requestDisallowInterceptTouchEvent(true);
+                return false;
+            }
+        });
+        musicList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                musicPosition = position;
+                changeMusic(position);
+            }
+        });
+    }
+
+
+
+
+    private void setAnimator() {
+        animator = ObjectAnimator.ofFloat(imageView, "rotation", 0f, 360.0f);
+        animator.setRepeatCount(0);
+        animator.setInterpolator(new LinearInterpolator());
+    }
+
+    private void connection() {
+        Intent intent = new Intent(this, MusicService.class);
+        startService(intent);
+        bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            Log.e("111","onServiceConnected");
+            musicService = ((MusicService.MyBinder) (service)).getService();
+            musicService.init();
+            seekBar.setMax(musicService.getMediaPlayer().getDuration());
+            handler.post(update);
+            animator.setDuration(musicService.getMediaPlayer().getDuration());
+            source.setText(music.get(0).getMusicSource());
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            Log.e("111","onServiceDisconnected");
+            musicService = null;
+        }
+
+    };
+
+    private void setSeekBarOption() {
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(final SeekBar seekBar, int progress, boolean fromUser) {}
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                musicService.getMediaPlayer().seekTo(seekBar.getProgress());
+                if (animator.isStarted()) {
+                    animator.end();
+                }
+                animator.setDuration(musicService.getMediaPlayer().getDuration() -
+                        musicService.getMediaPlayer().getCurrentPosition());
+                if (musicService.getMediaPlayer().isPlaying()) {
+                    animator.start();
+                }
+            }
+        });
+    }
+
+    private void setNext_music() {
+        if (musicPosition == musicList.getCount() - 1) {
+            musicPosition = 0;
+        }
+        else {
+            musicPosition++;
+        }
+        changeMusic(musicPosition);
+    }
+
+    private void setLast_music() {
+        if (musicPosition != 0) {
+            musicPosition--;
+        }
+        changeMusic(musicPosition);
+    }
+
+    private void playAndPause() {
+        musicService.playAndPause();
+        if (isPlaying) {
+            playing.setText("暂停");
+            //play.setText(R.string.play);
+            isPlaying = false;
+            animator.pause();
+            imageButton.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.bofangzhuangtaitingzhi));
+        } else {
+            if (animator.isStarted()) {
+                animator.resume();
+            } else {
+                animator.setDuration(musicService.getMediaPlayer().getDuration());
+                animator.start();
+            }
+            playing.setText("播放中");
+            //play.setText(R.string.pause);
+            isPlaying = true;
+            handler.post(update);
+            imageButton.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.bofangzhuangtai));
+        }
+        changeWidget(music.get(musicPosition).getMusicName(), isPlaying);
+    }
+
+    private void stop() {
+//        handler.removeCallbacks(update);
+//        seekBar.setProgress(0);
+//        resTime.setText(time.format(musicService.getMediaPlayer().getDuration()));
+//        playTime.setText(time.format(0));
+//        playing.setText("R.string.stop");
+//        //play.setText(R.string.play);
+//        imageButton.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.bofangzhuangtaitingzhi));
+//        isPlaying = false;
+//        animator.end();
+//        musicService.stop();
+//        changeWidget(music.get(musicPosition).getMusicName(), isPlaying);
+        musicService.stop();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK ) {
+            isPlaying = false;
+            musicService.playAndPause();
+            finish();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private void changeMusic(int position) {
+        if (musicService.getMediaPlayer().isPlaying()) {
+            handler.removeCallbacks(update);
+            isPlaying = false;
+        }
+        if (position == 0) {
+            musicService.init();
+            musicService.getMediaPlayer().start();
+        }
+        else {
+            if (musicService.getMediaPlayer() != null) {
+                musicService.setSource(music.get(position).getMusicSource());
+                musicService.changeMusic();
+            }
+        }
+        seekBar.setMax(musicService.getMediaPlayer().getDuration());
+        if (animator.isStarted()) {
+            animator.end();
+        }
+        animator.setDuration(musicService.getMediaPlayer().getDuration());
+        animator.start();
+        handler.post(update);
+        isPlaying = true;
+        //play.setText(R.string.pause);
+        imageButton.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.bofangzhuangtai));
+        playing.setText("播放中");
+        source.setText(music.get(position).getMusicSource());
+        changeWidget(music.get(position).getMusicName(), isPlaying);
+    }
+
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(RecordActivity.musicWidget)) {
+                int action = intent.getIntExtra("musicOption", -1);
+                switch (action) {
+                    case 0:
+                        playAndPause();
+                        break;
+/*                    case 1:
+                        stop();
+                        break;*/
+                    case 2:
+                        setNext_music();
+                        break;
+                    case 3:
+                        setLast_music();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    };
+
+
+    private void changeWidget(String source, boolean isPlaying) {
+        Intent intent = new Intent(RecordActivity.changeWidget);
+        intent.putExtra("source", source);
+        intent.putExtra("isPlaying", isPlaying);
+        this.sendBroadcast(intent);
     }
 
 }
