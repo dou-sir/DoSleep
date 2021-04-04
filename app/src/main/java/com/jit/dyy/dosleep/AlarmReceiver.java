75ba50b5af8554c1fe956e43d71798ebb1169886
dou-sir <1403268725@qq.com>
2021-04-04 19:10:12 +0800

    test

---
+++ app/src/main/java/com/jit/dyy/dosleep/AlarmReceiver.java
@@ -0,0 +1,18 @@
+
+package com.jit.dyy.dosleep;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+	/* (non-Javadoc)
+	 * @see android.content.BroadcastReceiver#onReceive(android.content.Context, android.content.Intent)
+	 */
+	@Override
+	public void onReceive(Context arg0, Intent data) {
+//		Log.d(Alarm.TAG, "the time is up,start the alarm...");
+//		Toast.makeText(arg0, "闹钟时间到了！", Toast.LENGTH_SHORT).show();
+	}
+}
---
+++ app/src/main/java/com/jit/dyy/dosleep/WebviewActivity.java
@@ -0,0 +1,146 @@
+package com.jit.dyy.dosleep;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class WebviewActivity extends AppCompatActivity {
+
+    WebSettings webSettings;
+
+    @BindView(R.id.title)
+    TextView mtitle;
+    @BindView(R.id.text_beginLoading)
+    TextView textBeginLoading;
+    @BindView(R.id.text_Loading)
+    TextView textLoading;
+    @BindView(R.id.webView)
+    WebView webView;
+    @BindView(R.id.ibClose)
+    ImageButton ibClose;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+        ButterKnife.bind(this);
+        init();
+    }
+
+    @OnClick({R.id.ibClose})
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.ibClose:
+                this.finish();
+
+        }
+    }
+
+    private void init() {
+        //支持缩放
+        webView.getSettings().setSupportZoom(true);
+        //设置出现缩放工具
+        webView.getSettings().setBuiltInZoomControls(true);
+        //扩大比例的缩放
+        webView.getSettings().setUseWideViewPort(true);
+        //js交互
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        webView.getSettings().setLoadWithOverviewMode(true);
+
+        webSettings = webView.getSettings();
+        webView.loadUrl("http://dosleep.tech/");
+        //设置不用系统浏览器打开,直接显示在当前Webview
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        //设置WebChromeClient类
+        webView.setWebChromeClient(new WebChromeClient() {
+
+
+            //获取网站标题
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                System.out.println("标题在这里");
+                mtitle.setText(title);
+            }
+
+
+            //获取加载进度
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                if (newProgress < 100) {
+                    String progress = newProgress + "%";
+                    textLoading.setText(progress);
+                } else if (newProgress == 100) {
+                    String progress = newProgress + "%";
+                    textLoading.setText(progress);
+                }
+            }
+        });
+
+
+        //设置WebViewClient类
+        webView.setWebViewClient(new WebViewClient() {
+            //设置加载前的函数
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                System.out.println("开始加载了");
+                textBeginLoading.setText("开始加载了");
+
+            }
+
+            //设置结束加载函数
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                textBeginLoading.setText("结束加载了");
+
+            }
+        });
+
+    }
+
+    //点击返回上一页面而不是退出浏览器
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+    //销毁Webview
+    @Override
+    protected void onDestroy() {
+        if (webView != null) {
+            webView.loadDataWithBaseURL(null, "", "text/html", "utf-8", null);
+            webView.clearHistory();
+
+            ((ViewGroup) webView.getParent()).removeView(webView);
+            webView.destroy();
+            webView = null;
+        }
+        super.onDestroy();
+    }
+}
